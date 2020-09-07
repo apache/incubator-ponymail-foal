f0d8cacd620c756a109aea7708f8c92d238f1f64
Daniel Gruno <humbedooh@apache.org>
2020-09-07 22:56:54 +0200

    Make oauth process async using server runners

--- server/plugins/oauthGeneric.py
+++ server/plugins/oauthGeneric.py
@@ -3,16 +3,14 @@ import re
 import requests
 
 
-def process(formdata, session):
+async def process(formdata, session, server):
     js = None
     m = re.match(r"https?://(.+)/", formdata["oauth_token"])
     if m:
         oauth_domain = m.group(1)
         headers = {"User-Agent": "Pony Mail OAuth Agent/0.1"}
-        rv = requests.post(formdata["oauth_token"], headers=headers, data=formdata)
-        # try:
+        rv = await server.runners.run(requests.post, formdata["oauth_token"], headers=headers, data=formdata)
         js = rv.json()
         js["oauth_domain"] = oauth_domain
         js['authoritative'] = True
-
     return js
