b566735816c91e27375a5a099ec77e946b843ca6
Daniel Gruno <humbedooh@apache.org>
2020-09-08 09:45:59 +0200

    drop a note about runners

--- server/plugins/oauthGeneric.py
+++ server/plugins/oauthGeneric.py
@@ -9,6 +9,7 @@ async def process(formdata, session, server):
     if m:
         oauth_domain = m.group(1)
         headers = {"User-Agent": "Pony Mail OAuth Agent/0.1"}
+        # This is a synchronous process, so we offload it to an async runner in order to let the main loop continue.
         rv = await server.runners.run(requests.post, formdata["oauth_token"], headers=headers, data=formdata)
         js = rv.json()
         js["oauth_domain"] = oauth_domain
--- server/plugins/oauthGoogle.py
+++ server/plugins/oauthGoogle.py
@@ -15,6 +15,7 @@ import plugins.session
 async def process(formdata, session, server: plugins.server.BaseServer):
     js = None
     request = google.auth.transport.requests.Request()
+    # This is a synchronous process, so we offload it to an async runner in order to let the main loop continue.
     id_info = await server.runners.run(
         google.oauth2.id_token.verify_oauth2_token,
         formdata.get("id_token"),
