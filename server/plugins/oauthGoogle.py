5b5ee7c2ff58993987372cbe34ee7917ab6be327
Daniel Gruno <humbedooh@apache.org>
2020-09-08 09:19:27 +0200

    clean up module syntax

--- server/plugins/oauthGoogle.py
+++ server/plugins/oauthGoogle.py
@@ -6,23 +6,21 @@ oauth:
   google_client_id:    your-client-id-here
 
 """
+import google.auth.transport.requests
+import google.oauth2.id_token
 import plugins.server
 import plugins.session
-
-import requests
-from google.oauth2 import id_token
-from google.auth.transport import requests
 
 
 async def process(formdata, session, server: plugins.server.BaseServer):
     js = None
-    request = requests.Request()
-
-    id_info = await server.runners.run(id_token.verify_oauth2_token,
-                                       formdata.get("id_token"),
-                                       request,
-                                       server.config.oauth.google_client_id
-                                       )
+    request = google.auth.transport.requests.Request()
+    id_info = await server.runners.run(
+        google.oauth2.id_token.verify_oauth2_token,
+        formdata.get("id_token"),
+        request,
+        server.config.oauth.google_client_id,
+    )
 
     if id_info and "email" in id_info:
         js = {
