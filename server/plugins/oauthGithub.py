87d70b72c025defd8ecc81c477f707ee7b9a526a
Daniel Gruno <humbedooh@apache.org>
2020-09-08 10:31:42 +0200

    Switch to using token headers (non-deprecated), lint

--- server/plugins/oauthGithub.py
+++ server/plugins/oauthGithub.py
@@ -14,15 +14,23 @@ import plugins.server
 import typing
 
 
-async def process(formdata, session, server: plugins.server.BaseServer) -> typing.Optional[dict]:
-    formdata['client_id'] = server.config.oauth.github_client_id
-    formdata['client_secret'] = server.config.oauth.github_client_secret
+async def process(
+    formdata, session, server: plugins.server.BaseServer
+) -> typing.Optional[dict]:
+    formdata["client_id"] = server.config.oauth.github_client_id
+    formdata["client_secret"] = server.config.oauth.github_client_secret
 
-    rv = await server.runners.run(requests.post, 'https://github.com/login/oauth/access_token', data=formdata)
-    m = re.search(r"(access_token=[a-f0-9]+)", rv.text)
+    rv = await server.runners.run(
+        requests.post, "https://github.com/login/oauth/access_token", data=formdata
+    )
+    m = re.search(r"access_token=([a-f0-9]+)", rv.text)
 
     if m:
-        rv = await server.runners.run(requests.get, "https://api.github.com/user?%s" % m.group(1))
+        rv = await server.runners.run(
+            requests.get,
+            "https://api.github.com/user",
+            headers={"authorization": "token %s" % m.group(1)},
+        )
         js = rv.json()
         js["oauth_domain"] = "github.com"
         return js
