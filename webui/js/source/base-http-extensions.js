05070b9b21adbe733de16107fb444bdc94aa2f44
Sebb <sebb@apache.org>
2021-11-01 00:17:10 +0000

    Typo: resoruce => resource

--- webui/js/source/base-http-extensions.js
+++ webui/js/source/base-http-extensions.js
@@ -67,7 +67,7 @@ async function async_snap(error) {
     msg = await error.text();
     msg = msg.replace(/<.*?>/g, ""); // strip HTML tags
     if (error.status === 404) {
-        msg += "\n\nYou may need to be logged in with additional permissions in order to view this resoruce.";
+        msg += "\n\nYou may need to be logged in with additional permissions in order to view this resource.";
     }
     modal("An error occured", "An error code %u occured while trying to fetch %s:\n%s".format(error.status, error.url, msg), "error");
 }
