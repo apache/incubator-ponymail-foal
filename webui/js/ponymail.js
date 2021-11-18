3aca08284fe57a6f0c2d54a50b81061b05488182
Sebb <sebb@apache.org>
2021-11-18 00:06:59 +0000

    init must come first to ensure vars are set up

--- webui/js/ponymail.js
+++ webui/js/ponymail.js
@@ -16,8 +16,97 @@
 */
 // THIS IS AN AUTOMATICALLY COMBINED FILE. PLEASE EDIT source/*.js!!
 
-const PONYMAIL_REVISION = "667f270";
+const PONYMAIL_REVISION = "1cff7c7";
 
+
+
+/******************************************
+ Fetched from source/aainit.js
+******************************************/
+
+const PONYMAIL_VERSION = "1.0.1"; // Current version of Pony Mail Foal
+
+let apiURL = ''; // external API URL. Usually left blank.
+
+// Stuff regarding what we're doing right now
+let current_json = {};
+let current_state = {};
+let current_list = '';
+let current_domain = '';
+let current_year = 0;
+let current_month = 0;
+let current_quick_search = '';
+let current_query = '';
+let select_primed = false;
+let ponymail_preferences = {};
+let ponymail_search_list = 'this';
+
+let current_listmode = 'threaded';
+let ponymail_max_nesting = 10; // max nesting level before unthreading to save space
+
+// thread state
+let current_email_idx = undefined;
+let chatty_layout = true;
+let ponymail_date_format = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+};
+let collated_json = {};
+
+console.log("/******* Apache Pony Mail (Foal v/%s) Initializing ********/".format(PONYMAIL_VERSION))
+// Adjust titles:
+document.title = prefs.title;
+for (let title of document.getElementsByClassName("title")) {
+    title.innerText = prefs.title;
+}
+
+// check local storage for settings
+console.log("Checking localStorage availability");
+let can_store = false;
+if (window.localStorage && window.localStorage.setItem) {
+    try {
+        window.localStorage.setItem("ponymail_test", "foo");
+        can_store = true;
+        console.log("localStorage available!");
+    } catch (e) {
+        console.log("no localStorage available!");
+    }
+}
+
+
+console.log("Initializing escrow checks");
+window.setInterval(escrow_check, 250);
+
+console.log("Initializing key command logger");
+window.addEventListener('keyup', keyCommands);
+
+if (pm_config.apiURL) {
+    apiURL = pm_config.apiURL;
+    console.log("Setting API URL to %s".format(apiURL));
+}
+
+window.addEventListener('load', function() {
+    document.body.appendChild(new HTML('footer', {
+        class: 'footer'
+    }, [
+        new HTML('div', {
+            class: 'container'
+        }, [
+            new HTML('p', {
+                class: 'muted'
+            }, "Powered by Apache Pony Mail (Foal v/%s ~%s)".format(PONYMAIL_VERSION, PONYMAIL_REVISION))
+        ])
+    ]));
+});
+console.log("initializing pop state checker");
+window.onpopstate = function(event) {
+    console.log("Popping state");
+    return parseURL({
+        cached: true
+    });
+};
 
 
 /******************************************
@@ -1680,95 +1769,6 @@ function showCalendarPicker(parent, seedDate) {
 
     drawCalendarPicker(div, seedDate)
 }
-
-
-/******************************************
- Fetched from source/init.js
-******************************************/
-
-const PONYMAIL_VERSION = "1.0.1"; // Current version of Pony Mail Foal
-
-let apiURL = ''; // external API URL. Usually left blank.
-
-// Stuff regarding what we're doing right now
-let current_json = {};
-let current_state = {};
-let current_list = '';
-let current_domain = '';
-let current_year = 0;
-let current_month = 0;
-let current_quick_search = '';
-let current_query = '';
-let select_primed = false;
-let ponymail_preferences = {};
-let ponymail_search_list = 'this';
-
-let current_listmode = 'threaded';
-let ponymail_max_nesting = 10; // max nesting level before unthreading to save space
-
-// thread state
-let current_email_idx = undefined;
-let chatty_layout = true;
-let ponymail_date_format = {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-};
-let collated_json = {};
-
-console.log("/******* Apache Pony Mail (Foal v/%s) Initializing ********/".format(PONYMAIL_VERSION))
-// Adjust titles:
-document.title = prefs.title;
-for (let title of document.getElementsByClassName("title")) {
-    title.innerText = prefs.title;
-}
-
-// check local storage for settings
-console.log("Checking localStorage availability");
-let can_store = false;
-if (window.localStorage && window.localStorage.setItem) {
-    try {
-        window.localStorage.setItem("ponymail_test", "foo");
-        can_store = true;
-        console.log("localStorage available!");
-    } catch (e) {
-        console.log("no localStorage available!");
-    }
-}
-
-
-console.log("Initializing escrow checks");
-window.setInterval(escrow_check, 250);
-
-console.log("Initializing key command logger");
-window.addEventListener('keyup', keyCommands);
-
-if (pm_config.apiURL) {
-    apiURL = pm_config.apiURL;
-    console.log("Setting API URL to %s".format(apiURL));
-}
-
-window.addEventListener('load', function() {
-    document.body.appendChild(new HTML('footer', {
-        class: 'footer'
-    }, [
-        new HTML('div', {
-            class: 'container'
-        }, [
-            new HTML('p', {
-                class: 'muted'
-            }, "Powered by Apache Pony Mail (Foal v/%s ~%s)".format(PONYMAIL_VERSION, PONYMAIL_REVISION))
-        ])
-    ]));
-});
-console.log("initializing pop state checker");
-window.onpopstate = function(event) {
-    console.log("Popping state");
-    return parseURL({
-        cached: true
-    });
-};
 
 
 /******************************************
