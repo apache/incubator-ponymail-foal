fd5f49099d7e034e6c8188f7f4436ae061724986
Sebb <sebb@apache.org>
2022-01-29 16:49:19 +0000

    Tests of management interface

--- test/itest_integration.py
+++ test/itest_integration.py
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
 import pytest
 import random
 import requests
@@ -22,6 +23,12 @@ import requests
 # Run as: python3 -m pytest [-s] test/itest_integration.py
 
 API_BASE='http://localhost:8080/api'
+TEST_DOMAIN = 'ponymail.apache.org'
+TEST_LIST = 'users'
+TEST_LIST2 = 'dev'
+DOCUMENT_HIDE_TEST = "c396ps3p5pb05srb4269dzcg9j7sof42"
+DOCUMENT_EDIT_TEST = "ffc3s2wzpn4n4pfonk9rffs4mnbk3l65" # dev list
+DOCUMENT_EDIT_SOURCE = "a05f5a472b5e7e6d0ea10162fa9d2b499861258c142dbb7f402454ad23b4af46"
 
 # Emulate how test auth is used by GUI
 def get_cookies(user='user'):
@@ -37,77 +44,108 @@ def get_cookies(user='user'):
     assert 'credentials' in jzon['login']
     return cookies
 
+def check_email(email, cookies):
+    # check email accessibility
+    mid = email['mid']
+    private = email['private']
+
+    # access by Permalink
+    res = requests.get(
+        f"{API_BASE}/email.lua",
+        params={"id": mid},
+        cookies=cookies
+    )
+    assert res.status_code == 200, mid
+    jzon = res.json()
+    assert mid == jzon['mid']
+    assert mid in jzon['permalinks']
+
+    # check email access by message-id
+    msgid = jzon['message-id']
+    listid = jzon['list_raw']
+    res = requests.get(
+        f"{API_BASE}/email.lua",
+        params={"id": msgid, "listid": listid},
+        cookies=cookies
+    )
+    assert res.status_code == 200, msgid
+    if private:
+        # should not be visible without cookies
+        res = requests.get(
+            f"{API_BASE}/email.lua",
+            params={"id": mid}
+        )
+        assert res.status_code == 404, mid
+        res = requests.get(
+            f"{API_BASE}/email.lua",
+            params={"id": msgid, "listid": listid}
+        )
+        assert res.status_code == 404, msgid
+    return mid, msgid, listid, private
+
+def check_source(mid, msgid, listid, private, cookies):
+    res = requests.get(
+        f"{API_BASE}/source.lua",
+        params={"id": mid},
+        cookies=cookies
+    )
+    assert res.status_code == 200, mid
+    res = requests.get(
+        f"{API_BASE}/source.lua",
+        params={"id": msgid, "listid": listid},
+        cookies=cookies
+    )
+    assert res.status_code == 200, mid
+    if private:
+        # should not be visible without cookies
+        res = requests.get(
+            f"{API_BASE}/source.lua",
+            params={"id": mid}
+        )
+        assert res.status_code == 404, mid
+        res = requests.get(
+            f"{API_BASE}/source.lua",
+            params={"id": msgid, "listid": listid}
+        )
+        assert res.status_code == 404, mid
+
 def check_access(email, cookies):
-        # check email accessibility
-        mid = email['mid']
-        res = requests.get(
-            f"{API_BASE}/email.lua",
-            params={"id": mid},
-            cookies=cookies
+        mid, msgid, listid, private = check_email(email, cookies)
+        check_source(mid, msgid, listid, private, cookies)
+
+def test_setup():
+    # ensure test conditions are correct at the start
+    import yaml
+    yaml = yaml.safe_load(open("server/ponymail.yaml"))
+    dburl = yaml['database']['dburl']
+    from requests.compat import urljoin
+    path = urljoin(dburl, "ponymail-auditlog/_delete_by_query")
+    res = requests.post(
+        path,
+        json={ "query": { "match_all": {} }},
+        headers={"Content-Type": 'application/json'}
         )
-        assert res.status_code == 200
-        jzon = res.json()
-        assert mid == jzon['mid']
-        assert mid in jzon['permalinks']
-        # check email access by message-id
-        msgid = jzon['message-id']
-        listid = jzon['list_raw']
-        res = requests.get(
-            f"{API_BASE}/email.lua",
-            params={"id": msgid, "listid": listid},
-            cookies=cookies
+    assert res.status_code == 200
+    path = urljoin(dburl, f"ponymail-source/_update/{DOCUMENT_EDIT_SOURCE}")
+    res = requests.post(
+        path,
+        json={ "doc": {"deleted": False} },
+        headers={"Content-Type": 'application/json'}
         )
-        assert res.status_code == 200
-        if email['private']:
-            # should not be visible without cookies
-            res = requests.get(
-                f"{API_BASE}/email.lua",
-                params={"id": mid}
-            )
-            assert res.status_code == 404
-            res = requests.get(
-                f"{API_BASE}/email.lua",
-                params={"id": msgid, "listid": listid}
-            )
-            assert res.status_code == 404
-        # check source accessibility
-        res = requests.get(
-            f"{API_BASE}/source.lua",
-            params={"id": mid},
-            cookies=cookies
-        )
-        assert res.status_code == 200
-        res = requests.get(
-            f"{API_BASE}/source.lua",
-            params={"id": msgid, "listid": listid},
-            cookies=cookies
-        )
-        assert res.status_code == 200
-        if email['private']:
-            # should not be visible without cookies
-            res = requests.get(
-                f"{API_BASE}/source.lua",
-                params={"id": mid}
-            )
-            assert res.status_code == 404
-            res = requests.get(
-                f"{API_BASE}/source.lua",
-                params={"id": msgid, "listid": listid}
-            )
-            assert res.status_code == 404
+    assert res.status_code == 200
 
 def test_lists():
     jzon = requests.get(f"{API_BASE}/preferences").json()
     # print(jzon)
     lists = jzon['lists']
-    assert 'ponymail.apache.org' in lists
-    assert 'users' in lists['ponymail.apache.org']
+    assert TEST_DOMAIN in lists
+    assert TEST_LIST in lists[TEST_DOMAIN]
     assert len(lists) == 1 # only expecting one domain
 
 def test_public_stats():
     jzon = requests.get(
         f"{API_BASE}/stats.lua",
-        params={"list": 'users', "domain": 'ponymail.apache.org', "emailsOnly": True, "d": 'gte=0d'}
+        params={"list": TEST_LIST, "domain": TEST_DOMAIN, "emailsOnly": True, "d": 'gte=0d'}
     ).json()
     assert jzon['firstYear'] == 2022
     assert jzon['firstMonth'] == 1
@@ -123,7 +161,7 @@ def test_public_stats():
     # Check we cannot see the private emails
     jzon = requests.get(
         f"{API_BASE}/stats.lua",
-        params={"list": 'users', "domain": 'ponymail.apache.org', "emailsOnly": True, "d": '2019-09'}
+        params={"list": TEST_LIST, "domain": TEST_DOMAIN, "emailsOnly": True, "d": '2019-09'}
         ).json()
     assert jzon['hits'] == 0
 
@@ -132,7 +170,7 @@ def test_private_stats():
     # only fetch the private mail stats
     jzon = requests.get(
         f"{API_BASE}/stats.lua",
-        params={"list": 'users', "domain": 'ponymail.apache.org', "emailsOnly": True, "d": '2019-09'},
+        params={"list": TEST_LIST, "domain": TEST_DOMAIN, "emailsOnly": True, "d": '2019-09'},
         cookies=cookies
     ).json()
     # The earlier mails are private
@@ -197,3 +235,109 @@ def test_mgmt_validation():
         {"action": 'edit', "document": '1234', "from": 'sender', "subject": 'Test Email', "list": 'abc', "body": 'body'},
         admin_cookies, 404)
     assert "Email not found!" in text
+
+def test_mgmt_log_before():
+    admin_cookies = get_cookies('admin')
+    jzon = mgmt_get_json({"action": 'log'}, admin_cookies)
+    assert len(jzon['entries']) == 0
+
+def test_mgmt_hiding():
+    admin_cookies = get_cookies('admin')
+
+    # reset in case of earlier failure
+    text = mgmt_get_text({"action": 'unhide', "document": DOCUMENT_HIDE_TEST}, admin_cookies)
+    assert text == "Unhid 1 emails from archives."
+
+    jzon = requests.get(
+        f"{API_BASE}/stats.lua",
+        params={"list": TEST_LIST, "domain": TEST_DOMAIN, "emailsOnly": True, "d": 'gte=0d'}
+    ).json()
+
+    assert jzon['hits'] == 6
+
+    check_access({"mid": DOCUMENT_HIDE_TEST, "private": False}, admin_cookies)
+
+    text = mgmt_get_text({"action": 'hide', "document": DOCUMENT_HIDE_TEST}, admin_cookies)
+    assert text == "Hid 1 emails from archives."
+
+    jzon = requests.get(
+        f"{API_BASE}/stats.lua",
+        params={"list": TEST_LIST, "domain": TEST_DOMAIN, "emailsOnly": True, "d": 'gte=0d'}
+    ).json()
+    assert jzon['hits'] == 5
+
+
+
+    text = mgmt_get_text({"action": 'unhide', "document": DOCUMENT_HIDE_TEST}, admin_cookies)
+    assert text == "Unhid 1 emails from archives."
+
+    jzon = requests.get(
+        f"{API_BASE}/stats.lua",
+        params={"list": TEST_LIST, "domain": TEST_DOMAIN, "emailsOnly": True, "d": 'gte=0d'}
+    ).json()
+    assert jzon['hits'] == 6
+
+    check_access({"mid": DOCUMENT_HIDE_TEST, "private": False}, None)
+
+def test_mgmt_edit():
+    """This test causes the source for an entry to be hidden"""
+    admin_cookies = get_cookies('admin')
+
+    test_list_id = f"<{TEST_LIST2}.{TEST_DOMAIN}>"
+    jzon = requests.get(
+        f"{API_BASE}/stats.lua",
+        params={"list": TEST_LIST2, "domain": TEST_DOMAIN, "emailsOnly": True, "d": 'gte=0d'}
+    ).json()
+
+    assert jzon['hits'] == 1
+
+    email = jzon['emails'][0]
+    check_access(email, None) # should be fully accessible
+
+    res = requests.get(
+        f"{API_BASE}/mbox.lua",
+        params={"list": TEST_LIST2, "domain": TEST_DOMAIN, "d": '2020-10'}
+    )
+    assert res.status_code == 200
+    assert res.text.startswith('From dev-return-')
+
+    # N.B. use variable body so it is always changed, even after a reset
+    text = mgmt_get_text(
+        {
+            "action": 'edit', "document": DOCUMENT_EDIT_TEST,
+            "from": '', "subject": '', "list": test_list_id, "body": time.time(), "private": False,
+        },
+        admin_cookies
+        )
+    assert text == "Email successfully saved"
+
+    jzon = requests.get(
+        f"{API_BASE}/stats.lua",
+        params={"list": TEST_LIST2, "domain": TEST_DOMAIN, "emailsOnly": True, "d": 'gte=0d'}
+    ).json()
+
+    assert jzon['hits'] == 1 # mbox entry still accessible
+
+    check_email(email, None)
+    check_access(email, admin_cookies) # but the source needs admin
+
+    # check that cannot see mbox
+    res = requests.get(
+        f"{API_BASE}/mbox.lua",
+        params={"list": TEST_LIST2, "domain": TEST_DOMAIN, "d": '2020-10'}
+    )
+    assert res.status_code == 200
+    assert len(res.text) <= 1 # probably just LF
+
+    res = requests.get(
+        f"{API_BASE}/mbox.lua",
+        params={"list": TEST_LIST2, "domain": TEST_DOMAIN, "d": '2020-10'},
+        cookies=admin_cookies
+    )
+    assert res.status_code == 200
+    assert res.text.startswith('From dev-return-')
+
+def test_mgmt_log_after():
+    admin_cookies = get_cookies('admin')
+    jzon = mgmt_get_json({"action": 'log'}, admin_cookies)
+    assert len(jzon['entries']) == 4
